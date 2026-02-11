999d3a4f9a4e40e4c0a43861ddfe4dc54a58d18f
Javad Kazemi <j.kazemi@parityqc.com>
2026-02-11 20:00:01 +0100

    fix: adapt optimization engine to ParametrizedGate

--- src/rydopt/gates/__init__.py
+++ src/rydopt/gates/__init__.py
@@ -1,6 +1,7 @@
 from rydopt.gates.four_qubit_gate_asym import FourQubitGateAsym
 from rydopt.gates.four_qubit_gate_pyramidal import FourQubitGatePyramidal
 from rydopt.gates.three_qubit_gate_asym import ThreeQubitGateAsym
+from rydopt.gates.parametrized_gate import ParametrizedGate
 from rydopt.gates.three_qubit_gate_isosceles import ThreeQubitGateIsosceles
 from rydopt.gates.two_qubit_gate import TwoQubitGate
 from rydopt.gates.two_qubit_gate_asym import TwoQubitGateAsym
@@ -12,4 +13,5 @@ __all__ = [
     "ThreeQubitGateAsym",
     "FourQubitGatePyramidal",
     "FourQubitGateAsym",
+    "ParametrizedGate",
 ]
