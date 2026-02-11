622b12371956db1024082a9e0517a22add5ec9aa
Javad Kazemi <j.kazemi@parityqc.com>
2026-02-11 20:00:01 +0100

    fix: adapt optimization engine to ParametrizedGate

--- src/rydopt/characterization/__init__.py
+++ src/rydopt/characterization/__init__.py
@@ -1,12 +1,13 @@
 from rydopt.characterization.gate_performance import analyze_gate, analyze_gate_qutip
 from rydopt.characterization.plot_optimization_history import plot_optimization_history
-from rydopt.characterization.plot_pulse import plot_pulse
+from rydopt.characterization.plot_pulse import plot_mapped_pulse, plot_pulse
 from rydopt.characterization.plot_spectrum import plot_spectrum
 
 __all__ = [
     "analyze_gate",
     "analyze_gate_qutip",
     "plot_pulse",
+    "plot_mapped_pulse",
     "plot_spectrum",
     "plot_optimization_history",
 ]
